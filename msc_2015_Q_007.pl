4c7964d0dc70dedad527301494908cf1f280c4fa
smirkh03 <smirkh03@dcs.bbk.ac.uk>
2018-05-27 19:19:30 +0100

    added trace from prolog

--- msc_2015_Q_007.pl
+++ msc_2015_Q_007.pl
@@ -16,7 +16,19 @@ c(X,X) :- b(X).
 % the result of the race is:
 % A = B, B = 1 ;
 % A = B, B = a ;
+% A = 1, B = 2 ;
+% A = 1, B = 3.
+% full trace
+%[debug]  ?- c(A,B).
+%  T Call: (7) c(_G3256, _G3257)
+%  T Exit: (7) c(1, 1)
+% A = B, B = 1 ;
+%  T Exit: (7) c(a, a)
+% A = B, B = a ;
+%  T Redo: (7) c(_G3256, _G3257)
+%  T Exit: (7) c(1, 2)
 % A = 1,
 % B = 2 ;
+%  T Exit: (7) c(1, 3)
 % A = 1,
 % B = 3.
